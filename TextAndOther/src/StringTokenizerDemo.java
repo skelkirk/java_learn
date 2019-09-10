94c42d7e026f76eaf26a475eb5e7c41c7a79c9bb
skelkirk <nikolas217@mail.ru>
2019-09-10 22:38:18 +0500

    nextToken print token

---
+++ TextAndOther/src/StringTokenizerDemo.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class StringTokenizerDemo{
+    public static void main(String[] args){
+        // Базовый текст:
+        String text="Очень простой текст: состоит из слов, и без цифр!";
+        // Объект класса StringTokenizer:
+        StringTokenizer str=new StringTokenizer(text);
+        // Текстовая переменная:
+        String s;
+        while(str.hasMoreTokens()){
+            // "Считывание" очередного слова:
+            s=str.nextToken();
+            //s=str.nextToken(" :,!");
+            //s=str.nextToken(":,!");
+            // Вывод слова на экран:
+            System.out.println(s);}
+    }
+}
